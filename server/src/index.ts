cd652a7f75117eeb6af74d66986d5421f94ed032
NashIvan-cmd <NashIvanRamos@gmail.com>
2025-09-20 17:17:45 +0800

    index ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import { errorHandler } from './middlewares/error.middleware';
 
 // Load .env variables
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(helmet());
 app.use(cors());
@@ -29,3 +30,10 @@ app.get('/health', (req, res) => {
     timestamp: new Date().toISOString()
   });
 });
+
+// Should be after all routes
+app.use(errorHandler);
+// sa
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
